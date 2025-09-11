fd17b994b16058f9f1f698553fa226a3698367b6
Antero Voutilainen <antero.vouti@outlook.com>
2025-09-11 16:49:47 +0300

    Demo2 ex2

---
+++ demo2/AV_demo2_timesums.py
@@ -0,0 +1,48 @@
+'''
+Copyright (c) 2025 Antero Voutilainen
+Created: 11 09 2025
+
+Description: Time numpy and python sum.
+'''
+import numpy as np
+import time
+
+
+def time_operation(A, c=1):
+    """Prints used time from selected method.
+
+    Args:
+        A (array): Array containing summable numbers.
+        c (int, optional): numpy or python. Defaults to 1 (numpy).
+    """    
+    if c == 1:
+        start = time.time()
+        sum_value = np.sum(A)
+        end = time.time()
+        print(f"Sum using numpy: {sum_value}")
+        print(f"Summing time using numpy: {end - start}")
+    
+    if c == 2:
+        start = time.time()
+        sum_value = sum(A)
+        end = time.time()
+        print(f"Sum using python: {sum_value}")
+        print(f"Summing time using python: {end - start}")
+
+    return
+
+
+def main():
+
+    N = 10000000
+    Array = np.random.random(N)
+
+    time_operation(Array, 1)
+    time_operation(Array, 2)
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
+
